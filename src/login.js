052f0951fb6d61520c2c3862aa69714f931403fa
Nick <maisadzenika@gmail.com>
2024-02-28 18:51:21 +0400

    Update login form with error message and styling

--- src/login.js
+++ src/login.js
@@ -1,6 +1,11 @@
+
+
 async function login() {
-	const username = document.getElementById("username").value;
-	const password = document.getElementById("password").value;
+	const usernameElement = document.getElementById("username");
+	const passwordElement = document.getElementById("password");
+	const errorText = document.getElementById("error-text");
+	const username = usernameElement.value;
+	const password = passwordElement.value;
 	console.log(username);
 	console.log(password);
 
@@ -18,36 +23,58 @@ async function login() {
 
 	// ANYTHING AFTER THIS IS DONE IN NON TRADITIONAL WAY
 
-	const users = await (await fetch("https://fakestoreapi.com/users")).json();
-	users.forEach((user) => {
-		if (user.username === username && user.password === password) {
-			console.log("Logged in");
-			return true;
-		}
-	});
+	let usernameExists = false;
+	let passwordCorrect = false;
 
-	return true;
+	const users = await (await fetch("https://fakestoreapi.com/users")).json();
+
+	users.forEach((user) => {
+		if (user.username === username) {
+			console.log("Username is correct");
+			usernameExists = true;
+			if (user.password === password) {
+				passwordCorrect = true;
+				console.log("Password is correct");
+				errorText.innerHTML = "";
+				usernameElement.style.border = "2px solid black";
+				passwordElement.style.border = "2px solid black";
+				giveToken();
+			} 
+		} 
+	});
+	
+	if (!usernameExists) {
+		console.log("Username does not exist");
+		usernameElement.style.border = "2px solid red";
+		usernameElement.value = "";
+		passwordElement.value = "";
+		errorText.innerHTML = "Username does not exist";
+	} else if (!passwordCorrect){
+		usernameElement.style.border = "2px solid black";
+		passwordElement.style.border = "2px solid red";
+		passwordElement.value = "";
+		console.log("Password is incorrect");
+		errorText.innerHTML = "Password is incorrect";
+	}
+
+
 }
 
-async function giveToken() {
-	if (await login()) {
-		console.log("Token given");
-		// IN REALITY THE BACKEND WILL GIVE US A TOKEN
-		const token = {
-			token: "eyJhbGciOiJIUzI1NiIsInR",
-		};
-		localStorage.setItem(
-			"saocariOn_token",
-			JSON.stringify({ token: token, time: Date.now() })
-		);
-		window.open("index.html", "_self");
-	} else {
-		console.log("Wrong credentials");
-	}
+function giveToken() {
+	console.log("Token given");
+	// IN REALITY THE BACKEND WILL GIVE US A TOKEN
+	const token = {
+		token: "eyJhbGciOiJIUzI1NiIsInR",
+	};
+	localStorage.setItem(
+		"saocariOn_token",
+		JSON.stringify({ token: token, time: Date.now() })
+	);
+	window.open("index.html", "_self");
 }
 
 document.querySelector("form").addEventListener("submit", (e) => {
 	e.preventDefault();
+	login();
 	console.log("submit");
-	giveToken();
 });
